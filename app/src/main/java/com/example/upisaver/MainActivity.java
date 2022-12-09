de444cfe145880c89b29673fdaac051cb2548eb1
ShubhGupta2004 <shubh.gupta2021@vitstudent.ac.in>
2022-12-09 15:33:39 +0530

    basic design onto figma

--- app/src/main/java/com/example/upisaver/MainActivity.java
+++ app/src/main/java/com/example/upisaver/MainActivity.java
@@ -2,17 +2,25 @@ package com.example.upisaver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageButton add;
     @Override
-
-    
     protected void onCreate(Bundle savedInstanceState) {
-        
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        add=findViewById(R.id.addTrans);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //startActivity();
+            }
+        });
     }
 }
