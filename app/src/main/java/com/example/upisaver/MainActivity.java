1890da4dd309c557aac34d8156a6117bfaf3a2c4
ShubhGupta2004 <shubh.gupta2021@vitstudent.ac.in>
2022-12-09 16:23:04 +0530

    basic design by figma

--- app/src/main/java/com/example/upisaver/MainActivity.java
+++ app/src/main/java/com/example/upisaver/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
     ImageButton add;
+    boolean b = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +21,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //startActivity();
+                if(b) {
+                   // add.setImageResource(R.drawable.img_1);
+                    add.setBackgroundResource(R.drawable.img_1);
+                    b=false;
+                }else{
+                    add.setBackgroundResource(R.drawable.img);
+                    b=true;
+                }
             }
         });
     }
