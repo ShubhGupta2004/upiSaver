9d984468fb710fd5019bb9fc3f507a7b6e651d98
ShubhGupta2004 <shubh.gupta2021@vitstudent.ac.in>
2022-12-10 18:58:04 +0530

    bottom sheet with Major UI changes

--- app/src/main/java/com/example/upisaver/MainActivity.java
+++ app/src/main/java/com/example/upisaver/MainActivity.java
@@ -1,19 +1,48 @@
 package com.example.upisaver;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     ImageButton add;
     boolean b = true;
+    BottomSheetDialog bottomSheetDialog;
+    LinearLayout ll;
+    Switch inExp;
+    boolean saveOption = false;
+    ImageView imgIcon;
+    TextView textIt;
+    EditText changeEdit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomSheetDialog= new BottomSheetDialog(MainActivity.this,R.style.BottomSheet);
+        View v = LayoutInflater.from(MainActivity.this).inflate(R.layout.bottom_sheet_dialog,(LinearLayout)findViewById(R.id.bottomSheet));
+        bottomSheetDialog.setContentView(v);
         add=findViewById(R.id.addTrans);
+        ll=bottomSheetDialog.findViewById(R.id.saveIt);
+        inExp= bottomSheetDialog.findViewById(R.id.switch1);
+        changeEdit=bottomSheetDialog.findViewById(R.id.editTextNumber);
+        imgIcon=bottomSheetDialog.findViewById(R.id.moneyIcon);
+        textIt=bottomSheetDialog.findViewById(R.id.IncomeExpense);
+        
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,7 +56,38 @@ public class MainActivity extends AppCompatActivity {
                     add.setBackgroundResource(R.drawable.img);
                     b=true;
                 }
+
+                bottomSheetDialog.show();
+            }
+        });
+        ll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str = changeEdit.getText().toString();
+                int num = Integer.parseInt(str);
+                Toast.makeText(MainActivity.this,"Saved: "+num,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        inExp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(inExp.isChecked()){
+                    imgIcon.setImageResource(R.drawable.img_4);
+                    textIt.setText("Expense");
+                    saveOption=true;
+                }else{
+                    imgIcon.setImageResource(R.drawable.img_3);
+                    textIt.setText("Income");
+                    saveOption=false;
+                }
             }
         });
     }
+//    public void saveIt(View c){
+//        String str = changeEdit.getText().toString();
+//        int num = Integer.parseInt(str);
+//        Toast.makeText(MainActivity.this,"Saved: "+num,Toast.LENGTH_SHORT).show();
+//    }
+
 }
