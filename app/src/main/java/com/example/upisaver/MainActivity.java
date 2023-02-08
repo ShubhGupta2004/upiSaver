5f46f500dec752574e336b3b0cde17ec92a1ae24
ShubhGupta2004 <shubh.gupta2021@vitstudent.ac.in>
2023-02-08 16:07:56 +0530

    with some more debug 1.4

--- app/src/main/java/com/example/upisaver/MainActivity.java
+++ app/src/main/java/com/example/upisaver/MainActivity.java
@@ -26,6 +26,8 @@ import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.switchmaterial.SwitchMaterial;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Objects;
@@ -47,11 +49,14 @@ public class MainActivity extends AppCompatActivity {
     private final int SMS_Code = 1;
     ImageView imgIcon;
     TextView textIt;
+    TextView expenseTotal;
     EditText changeEdit;
     TextView transHeading;
     ImageButton img1;
     Button viewTransaction;
     Realm realm;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,19 +64,10 @@ public class MainActivity extends AppCompatActivity {
         Realm.init(getApplicationContext());
 
         //getting the view from layout
-        realm=Realm.getDefaultInstance();
-        bottomSheetDialog= new BottomSheetDialog(MainActivity.this,R.style.BottomSheet);
-        View v = LayoutInflater.from(MainActivity.this).inflate(R.layout.bottom_sheet_dialog,(LinearLayout)findViewById(R.id.bottomSheet));
-        bottomSheetDialog.setContentView(v);
-        add=findViewById(R.id.addTrans);
-        ll=bottomSheetDialog.findViewById(R.id.saveIt);
-        inExp= bottomSheetDialog.findViewById(R.id.switch1);
-        changeEdit=bottomSheetDialog.findViewById(R.id.editTextNumber);
-        imgIcon=bottomSheetDialog.findViewById(R.id.moneyIcon);
-        textIt=bottomSheetDialog.findViewById(R.id.IncomeExpense);
-        img1= findViewById(R.id.showSMS);
-        transHeading=findViewById(R.id.transanctionH);
-        viewTransaction=findViewById(R.id.viewTransactionButton);
+        initialization();
+
+        //setting up the value
+        setExpenseTotal();
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -139,19 +135,9 @@ public class MainActivity extends AppCompatActivity {
 
 
                 Toast.makeText(MainActivity.this,"Saved: "+num,Toast.LENGTH_SHORT).show();
-                realm.executeTransactionAsync(new Realm.Transaction(){
 
-                    @Override
-                    public void execute(@NonNull Realm realm) {
-                        RealmQuery<transaction> std = realm.where(transaction.class);
-                        RealmResults<transaction> rst = std.findAll();
-                        for (int i = 0; i < rst.size(); i++) {
-                            transaction tr = rst.get(i);
-                            assert tr != null;
-                            System.out.println(tr.getId()+" "+tr.getAmount()+" "+tr.getDate()+".");
-                        }
-                    }
-                });
+                printTransaction();
+
             }
         });
 
@@ -174,136 +160,34 @@ public class MainActivity extends AppCompatActivity {
         img1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                StringBuilder msg = new StringBuilder();
-                HashMap<Integer,String> msgDataTemp = new HashMap<>();
+                fetchMsg();
+            }
 
-                if(ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.READ_SMS)==PackageManager.PERMISSION_GRANTED){
-                    Cursor cursor = getContentResolver().query(Uri.parse("content://sms/inbox"), null, null, null, null);
-                    Number id2 = realm.where(transaction.class).max("msgId");
-                    if(id2==null){
-                        id2=0;
-                    }
-                    System.out.println("h"+id2);
-                    int id1 = Integer.parseInt(String.valueOf(id2));
-                    System.out.println(id1);
-                    boolean boom = true;
-                    if (cursor.moveToFirst()) { // must check the result to prevent exception
-                        try {
-                            do {
-                                String msgData = "";
-                                int idTemp = 0;
-                                String sTemp = "";
-                                String dateTemp = "";
-                                String amountTemp = "";
-                                for (int idx = 0; idx < cursor.getColumnCount(); idx++) {
-                                    msgData += " " + cursor.getColumnName(idx) + ":" + cursor.getString(idx);
-                                    if (Objects.equals(cursor.getColumnName(idx), "_id")) {
-                                        idTemp = Integer.parseInt(cursor.getString(idx));
 
-                                        if(idTemp<=(int)id1){
-                                            boom=false;
-                                            break;
-                                        }
-                                    }
-
-                                    if (Objects.equals(cursor.getColumnName(idx), "body")) {
-                                        String s = cursor.getString(idx);
-                                        if (s.contains("UPI")) {
-                                            sTemp = s;
-                                            if(s.contains("is debited for Rs.")){
-                                                int idx1 = s.indexOf("is debited for Rs.") + 18;
-                                                while (s.charAt(idx1) != ' ') {
-                                                    amountTemp += s.charAt(idx1);
-                                                    idx1++;
-                                                }
-                                            }else if(s.contains("debited by Rs")){
-                                                int idx1 = s.indexOf("debited by Rs") + 13;
-                                                while (s.charAt(idx1) != ' ') {
-                                                    amountTemp += s.charAt(idx1);
-                                                    idx1++;
-                                                }
-                                            }
-
-                                        }
-                                    }
-
-                                    if (Objects.equals(cursor.getColumnName(idx), "date")) {
-                                        dateTemp = cursor.getString(idx);
-                                    }
-                                }
-
-                                if (!sTemp.equals("")) {
-                                    msgDataTemp.put(idTemp, sTemp);
-                                }
-
-                                if (!amountTemp.equals("")) {
-                                    System.out.println(amountTemp);
-                                    float num = Float.parseFloat(amountTemp);
-                                    System.out.println(num);
-                                    long unix_seconds = Long.parseLong(dateTemp);
-
-                                    //convert seconds to milliseconds
-
-                                    Date date = new Date(unix_seconds);
-                                    // format of the date
-
-                                    SimpleDateFormat jdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-                                    TimeZone istTimeZone = TimeZone.getTimeZone("Asia/Kolkata");
-                                    jdf.setTimeZone(istTimeZone);
-                                    String date1 = jdf.format(date);
-                                    String usage = "Not defined";
-                                    Number id = realm.where(transaction.class).max("id");
-                                    int nextId;
-                                    // validating if id is null or not.
-
-                                    if (id == null) {
-                                        // if id is null
-                                        // we are passing it as 1.
-                                        nextId = 1;
-                                    } else {
-                                        // if id is not null then
-                                        // we are incrementing it by 1
-                                        nextId = id.intValue() + 1;
-                                    }
-
-                                    try {
-                                        realm.beginTransaction();
-                                        transaction trans = realm.createObject(transaction.class, nextId);
-                                        trans.setAmount((int)num);
-                                        trans.setDate(date1);
-                                        trans.setType(false);
-                                        trans.setUsage(usage);
-                                        trans.setMsgId(idTemp);
-                                        realm.commitTransaction();
-                                    } catch (Exception e) {
-                                        System.out.println(e.getMessage());
-                                        realm.cancelTransaction();
-                                    }
-                                }
-                                // use msgData
-                                if(!boom){
-                                    break;
-                                }
-                                msg.append(msgData).append("\n\n\n");
-
-                            } while (cursor.moveToNext());
-                            cursor.close();
-                        }catch (Exception e){
-                            Toast.makeText(MainActivity.this,"k"+e.getMessage(),Toast.LENGTH_SHORT).show();
-                            e.printStackTrace();
-                        }
-                    } else {
-                        Toast.makeText(MainActivity.this,"No msg",Toast.LENGTH_SHORT).show();
-                        // empty box, no SMS
-                    }
-                }else{
-                    requestSmsPermission();
-                }
-
-                transHeading.setText(msgDataTemp+" "+msg);
-
+            @Override
+            protected void finalize() throws Throwable {
+                super.finalize();
+                setExpenseTotal();
             }
         });
+
+    }
+
+    private void initialization() {
+        realm=Realm.getDefaultInstance();
+        bottomSheetDialog= new BottomSheetDialog(MainActivity.this,R.style.BottomSheet);
+        View v = LayoutInflater.from(MainActivity.this).inflate(R.layout.bottom_sheet_dialog,(LinearLayout)findViewById(R.id.bottomSheet));
+        bottomSheetDialog.setContentView(v);
+        add=findViewById(R.id.addTrans);
+        ll=bottomSheetDialog.findViewById(R.id.saveIt);
+        inExp= bottomSheetDialog.findViewById(R.id.switch1);
+        changeEdit=bottomSheetDialog.findViewById(R.id.editTextNumber);
+        imgIcon=bottomSheetDialog.findViewById(R.id.moneyIcon);
+        textIt=bottomSheetDialog.findViewById(R.id.IncomeExpense);
+        img1= findViewById(R.id.showSMS);
+        transHeading=findViewById(R.id.transanctionH);
+        viewTransaction=findViewById(R.id.viewTransactionButton);
+        expenseTotal=findViewById(R.id.expenseTotal);
     }
 
 
@@ -336,6 +220,188 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Permission Denied.", Toast.LENGTH_SHORT).show();
             }
         }
+    }
+
+
+    private void setExpenseTotal(){
+        ArrayList<Integer> arr = new ArrayList<>();
+        realm.executeTransactionAsync(new Realm.Transaction(){
+
+            @Override
+            public void execute(@NonNull Realm realm) {
+                RealmQuery<transaction> std = realm.where(transaction.class);
+                RealmResults<transaction> rst = std.findAll();
+                for (int i = 0; i < rst.size(); i++) {
+                    transaction tr = rst.get(i);
+                    assert tr != null;
+                    arr.add(tr.getAmount());
+                    System.out.println(tr.getId()+" "+tr.getAmount()+" "+tr.getDate()+".");
+                }
+                int expense = 0;
+                for(int i=0;i<arr.size();i++){
+                    expense+=arr.get(i);
+                }
+                System.out.println(Arrays.toString(arr.toArray()));
+                expenseTotal.setText(String.valueOf(expense));
+            }
+
+            @Override
+            protected void finalize() throws Throwable {
+                super.finalize();
+            }
+        });
+    }
+
+    private void printTransaction(){
+        realm.executeTransactionAsync(new Realm.Transaction(){
+
+            @Override
+            public void execute(@NonNull Realm realm) {
+                RealmQuery<transaction> std = realm.where(transaction.class);
+                RealmResults<transaction> rst = std.findAll();
+                for (int i = 0; i < rst.size(); i++) {
+                    transaction tr = rst.get(i);
+                    assert tr != null;
+                    System.out.println(tr.getId()+" "+tr.getAmount()+" "+tr.getDate()+".");
+                }
+            }
+
+            @Override
+            protected void finalize() throws Throwable {
+                super.finalize();
+                setExpenseTotal();
+            }
+        });
+    }
+
+    private void fetchMsg() {
+        StringBuilder msg = new StringBuilder();
+        HashMap<Integer,String> msgDataTemp = new HashMap<>();
+
+        if(ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.READ_SMS)==PackageManager.PERMISSION_GRANTED){
+            Cursor cursor = getContentResolver().query(Uri.parse("content://sms/inbox"), null, null, null, null);
+            Number id2 = realm.where(transaction.class).max("msgId");
+            if(id2==null){
+                id2=0;
+            }
+            System.out.println("h"+id2);
+            int id1 = Integer.parseInt(String.valueOf(id2));
+            System.out.println(id1);
+            boolean boom = true;
+            if (cursor.moveToFirst()) { // must check the result to prevent exception
+                try {
+                    do {
+                        String msgData = "";
+                        int idTemp = 0;
+                        String sTemp = "";
+                        String dateTemp = "";
+                        String amountTemp = "";
+                        for (int idx = 0; idx < cursor.getColumnCount(); idx++) {
+                            msgData += " " + cursor.getColumnName(idx) + ":" + cursor.getString(idx);
+                            if (Objects.equals(cursor.getColumnName(idx), "_id")) {
+                                idTemp = Integer.parseInt(cursor.getString(idx));
+
+                                if(idTemp<=(int)id1){
+                                    boom=false;
+                                    break;
+                                }
+                            }
+
+                            if (Objects.equals(cursor.getColumnName(idx), "body")) {
+                                String s = cursor.getString(idx);
+                                if (s.contains("UPI")) {
+                                    sTemp = s;
+                                    if(s.contains("is debited for Rs.")){
+                                        int idx1 = s.indexOf("is debited for Rs.") + 18;
+                                        while (s.charAt(idx1) != ' ') {
+                                            amountTemp += s.charAt(idx1);
+                                            idx1++;
+                                        }
+                                    }else if(s.contains("debited by Rs")){
+                                        int idx1 = s.indexOf("debited by Rs") + 13;
+                                        while (s.charAt(idx1) != ' ') {
+                                            amountTemp += s.charAt(idx1);
+                                            idx1++;
+                                        }
+                                    }
+
+                                }
+                            }
+
+                            if (Objects.equals(cursor.getColumnName(idx), "date")) {
+                                dateTemp = cursor.getString(idx);
+                            }
+                        }
+
+                        if (!sTemp.equals("")) {
+                            msgDataTemp.put(idTemp, sTemp);
+                        }
+
+                        if (!amountTemp.equals("")) {
+                            System.out.println(amountTemp);
+                            float num = Float.parseFloat(amountTemp);
+                            System.out.println(num);
+                            long unix_seconds = Long.parseLong(dateTemp);
+
+                            //convert seconds to milliseconds
+
+                            Date date = new Date(unix_seconds);
+                            // format of the date
+
+                            SimpleDateFormat jdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+                            TimeZone istTimeZone = TimeZone.getTimeZone("Asia/Kolkata");
+                            jdf.setTimeZone(istTimeZone);
+                            String date1 = jdf.format(date);
+                            String usage = "Not defined";
+                            Number id = realm.where(transaction.class).max("id");
+                            int nextId;
+                            // validating if id is null or not.
+
+                            if (id == null) {
+                                // if id is null
+                                // we are passing it as 1.
+                                nextId = 1;
+                            } else {
+                                // if id is not null then
+                                // we are incrementing it by 1
+                                nextId = id.intValue() + 1;
+                            }
+
+                            try {
+                                realm.beginTransaction();
+                                transaction trans = realm.createObject(transaction.class, nextId);
+                                trans.setAmount((int)num);
+                                trans.setDate(date1);
+                                trans.setType(false);
+                                trans.setUsage(usage);
+                                trans.setMsgId(idTemp);
+                                realm.commitTransaction();
+                            } catch (Exception e) {
+                                System.out.println(e.getMessage());
+                                realm.cancelTransaction();
+                            }
+                        }
+                        // use msgData
+                        if(!boom){
+                            break;
+                        }
+                        msg.append(msgData).append("\n\n\n");
+
+                    } while (cursor.moveToNext());
+                    cursor.close();
+                }catch (Exception e){
+                    Toast.makeText(MainActivity.this,"k"+e.getMessage(),Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+            } else {
+                Toast.makeText(MainActivity.this,"No msg",Toast.LENGTH_SHORT).show();
+                // empty box, no SMS
+            }
+        }else{
+            requestSmsPermission();
+        }
+        transHeading.setText(msgDataTemp+" "+msg);
+
     }
 
 }
